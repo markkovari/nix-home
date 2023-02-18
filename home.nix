ee25fba9768627343b61f34288337254db49720e
markkovari <kovarimarkofficial@gmail.com>
2023-02-18 20:13:21 +0100

    chore: add darwin based description

--- home.nix
+++ home.nix
@@ -1,25 +1,19 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
   home.username = "markkovari";
   home.homeDirectory = "/Users/markkovari";
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "22.11";
-
-  # Let Home Manager install and manage itself.
+  
   programs.home-manager.enable = true;
   programs.bat.enable = true;
   programs.gh.enable = true;
   programs.neovim.enable = true;
-  programs.fish.enable = true;
+  programs.zsh.enable = true;
+  programs.oh-my-posh.enable = true;
+
+  home.packages = [
+    pkgs.awscli
+  ];
 }
